a4722fc2c1a1ef4f6f939d1e5275a54c153d12f2
lub <github@lubiland.de>
2014-11-02 09:16:23 +0100

    fix service and cronjob

--- service.php
+++ service.php
@@ -1,8 +1,10 @@
 <?php
 require_once('config.inc.php');
+require_once('functions.php');
 require_once($framework);
+
 $server = TeamSpeak3::factory('serverquery://'.$user.':'.$passwd.'@'.$host.':'.$queryport.'/?server_port='.$voiceport);
 
 while(true) {
-	require_once('cronjob.php');
+	require_once('main.php');
 }
