cf1be9740af9d963413bea93935eeaebfe7575e1
lub <github@lubiland.de>
2014-11-02 09:00:45 +0100

    added running as a service instead of cronjob

---
+++ service.php
@@ -0,0 +1,8 @@
+<?php
+require_once('config.inc.php');
+require_once($framework);
+$server = TeamSpeak3::factory('serverquery://'.$user.':'.$passwd.'@'.$host.':'.$queryport.'/?server_port='.$voiceport);
+
+while(true) {
+	require_once('cronjob.php');
+}
